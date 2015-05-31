bdae48c85ffd7813f73edacef6018ccfdba96a26
Andrew Canby <andrewcanby@gmail.com>
2015-05-31 18:41:36 +1000

    Added annotation driven config for AOP

--- aop/src/main/java/com/canby/spring/aop/Camera.java
+++ aop/src/main/java/com/canby/spring/aop/Camera.java
@@ -1,8 +1,11 @@
 package com.canby.spring.aop;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by acanby on 31/05/2015.
  */
+@Component
 public class Camera {
 
     public void snap() {
--- aop/src/main/java/com/canby/spring/aop/Logger.java
+++ aop/src/main/java/com/canby/spring/aop/Logger.java
@@ -1,9 +1,17 @@
 package com.canby.spring.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by acanby on 31/05/2015.
  */
+@Component
+@Aspect
 public class Logger {
+
+    @Before("execution(void com.canby.spring.aop.Camera.snap())")
     public void aboutToTakePhoto() {
         System.out.println("About to take photo...");
     }
